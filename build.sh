4336b3218b948bb590b0e0085c383206b176ffe1
Denis Brækhus <denis@denis.no>
2023-01-04 14:40:03 +0100

    Use shasum tool.

--- build.sh
+++ build.sh
@@ -27,8 +27,8 @@ pushd ${OS}-${ARCH}
 [ -f cpython-3.9.14+20221002-${ARCH}-${DIST}-install_only.tar.gz ] || wget https://github.com/indygreg/python-build-standalone/releases/download/20221002/cpython-3.9.14+20221002-${ARCH}-${DIST}-install_only.tar.gz
 [ -f cpython-3.9.14+20221002-${ARCH}-${DIST}-install_only.tar.gz.sha256 ] || wget https://github.com/indygreg/python-build-standalone/releases/download/20221002/cpython-3.9.14+20221002-${ARCH}-${DIST}-install_only.tar.gz.sha256
 
-echo $(cat cpython-3.9.14+20221002-${ARCH}-${DIST}-install_only.tar.gz.sha256) cpython-3.9.14+20221002-${ARCH}-${DIST}-install_only.tar.gz | sha256sum --check || exit 1
-sha256sum --check scie-jump-${OS}-${ARCH}${EXE}.sha256 || exit 1
+echo $(cat cpython-3.9.14+20221002-${ARCH}-${DIST}-install_only.tar.gz.sha256) '*'cpython-3.9.14+20221002-${ARCH}-${DIST}-install_only.tar.gz | shasum -a 256 -c || exit 1
+shasum -a 256 -c scie-jump-${OS}-${ARCH}${EXE}.sha256 || exit 1
 chmod a+x scie-jump-${OS}-${ARCH}${EXE}
 popd
 
